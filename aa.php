99e2c1dcc03f6ce09726d8187af2fcd75119e1f0
SuperArya <herlambangharyo@live.com>
2020-08-21 11:36:46 +0700

    Create aa.php

---
+++ aa.php
@@ -0,0 +1,257 @@
+<div class="borderer"></div>
+	<div class="padding-20 font-roman yellow line-onehalf">
+		<div class="bold underline text-center font-size-16">
+			BERITA ACARA PEMASANGAN TUGU / TANDA BATAS
+		</div>
+
+		<br/>
+
+		<div class="text-justify">
+			Pada hari ini, <?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'39','','','............. tanggal ......... (.................................) Bulan ......... (.................................)	Tahun ........... (...........................................................)',1,'',1,'tanggal_tugu'); */ ?>
+
+			Kami yang bertanda tangan di bawah ini :
+		</div>
+
+		<table>
+			<colgroup>
+				<col width="">
+				<col width="20">
+				<col width="">
+			</colgroup>
+			
+			<tr>
+				<td class="menjorok">Nama</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id',$tipe_data_param_1,$parent_id_EX,$kode_unik_EX,NULL,'1','1','2','2',' / ','isi') ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="menjorok">Umur</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id',$tipe_data_param_1,$parent_id_EX,$kode_unik_EX,NULL,'1','1','2','4',' / ','isi') ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="menjorok">Alamat</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+			</tr>
+		</table>
+
+		<div class="text-justify">
+			Saya selaku pemilik tanah telah melaksanakan pemasangan tanda-tanda batas berupa (Tugu-tugu/besi) pada semua sudut bidang tanah terletak di :
+		</div>
+
+        <table>
+			<colgroup>
+				<col width="">
+				<col width="20">
+				<col width="">
+			</colgroup>
+			
+			<tr>
+				<td class="menjorok">Jalan</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="menjorok">RT / RW</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?> / 
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="menjorok">
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="menjorok">
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="menjorok">
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+				<td class="text-center">:</td>
+				<td>
+					<?php echo isi('id','data','0201','TGFkefa3Wi2jRH71KIEC',NULL,'1','1','2','2',' / ') ?>
+				</td>
+			</tr>
+		</table>
+
+		<ol class="text-justify">
+            <li>
+            	Bahwa tanda-tanda batas yang telah terpasang telah mendapat persetujuan dari pemilik tanah yang berbatasan.
+            </li>
+            <li>
+            	Bahwa untuk kesaksian tersebut para pemilik tanah yang berbatasan menandatangani Berita Acara ini.
+            </li>
+            <li>
+            	Bahwa tanah tersebut belum pernah diajukan permohonan sertipikat baik sebagian atau seluruhnya.
+            </li>        
+        </ol>
+
+        <div>
+			Demikian Berita Acara ini, dibuat dengan sebenar-benarnya tanpa paksaan.
+		</div>
+
+		<br/>
+
+        
+
+        <div class="float-left width-60">
+	        <div class="text-center">
+	        	Para pemilik tanah yang berbatasan
+	        </div>
+
+	        <table width="100%">
+				<colgroup>
+					<col width="14%">
+					<col width="2%">
+					<col width="50%">
+					<col width="2%">
+					<col width="30%">
+					<col width="2%">
+				</colgroup>
+				
+				<tr>
+					<td class="text-center"  colspan="3">Nama Pemilik Tanah</td>
+					
+					<td class="text-center" colspan="3"> 
+						<span>Tanda Tangan</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Utara
+					</td>
+					<td>:</td>
+					<td>
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'35','','','',1,'','','isi'); */ ?>
+					</td>
+					<td>(</td>
+					<td class="text-center">
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'35','','','',1,'','','batas_pengecualian_tugu'); */ ?>
+					</td>
+					<td>)</td>
+				</tr>
+				<tr>
+					<td>
+						Timur
+					</td>
+					<td>:</td>
+					<td>
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'36','','','',1,'','','isi'); */ ?>
+					</td>
+					<td>(</td>
+					<td class="text-center">
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'36','','','',1,'','','batas_pengecualian_tugu'); */ ?>
+					</td>
+					<td>)</td>
+				</tr>
+				<tr>
+					<td>
+						Selatan
+					</td>
+					<td>:</td>
+					<td>
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'37','','','',1,'','','isi'); */ ?>
+					</td>
+					<td>(</td>
+					<td class="text-center">
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'37','','','',1,'','','batas_pengecualian_tugu'); */ ?>
+					</td>
+					<td>)</td>
+				</tr>
+				<tr>
+					<td>
+						Barat
+					</td>
+					<td>:</td>
+					<td>
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'38','','','',1,'','','isi'); */ ?>
+					</td>
+					<td>(</td>
+					<td class="text-center">
+						<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,'38','','','',1,'','','batas_pengecualian_tugu'); */ ?>
+					</td>
+					<td>)</td>
+				</tr>
+			</table>
+
+			<br/>
+
+			<div>
+				Saksi-saksi / Perangkat Desa
+			</div>
+
+			<table>
+				<colgroup>
+					<col width="80%">
+                    <col width="20%">
+				</colgroup>
+				<tr>
+					<td colspan="2">
+						1. <?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,40,1,1,'',1,'','','isi'); */ ?>
+					</td>
+					<td>(.........................)</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						2. <?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,40,1,2,'',1,'','','isi'); */ ?>
+					</td>
+					<td>(.........................)</td>				
+				</tr>
+
+			</table>
+        </div>
+
+        <div class="float-right width-40">
+        	<div class="text-center">
+        		Pemohon,
+        	</div>
+        	<br/>
+        	<br/>
+        	<br/>
+        	<br/>
+        	<div class="bold uppercase text-center">
+        		<?php echo isi('id',$tipe_data_param_1,$parent_id_EX,$kode_unik_EX,NULL,'1','1','2','2',' / ','isi') ?>
+        	</div>
+        </div>
+
+        <div class="clear"></div>
+        <br/>
+		<div class="clear text-center">
+			Mengetahui<br/>
+			Kepala <?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,29,'','','',1,'','','wilayah'); */ ?> 
+			<br/>
+			
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<div class="bold uppercase text-center">
+				<?php /* echo isi_pb($kode_pengurusan_id,$kode_unik,NULL,42,'','','',1,'','','isi'); */ ?>
+			</div>
+		</div>
+
+		<div class="clear"></div>
+	</div>
