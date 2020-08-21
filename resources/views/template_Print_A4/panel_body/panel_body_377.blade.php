cb739b3783ec8f688b2e4cb84428e5f6243e16d2
Phoenix3264 <herlambangharyo@gmail.com>
2020-08-21 11:52:46 +0700

    up

---
+++ resources/views/template_Print_A4/panel_body/panel_body_377.blade.php
@@ -0,0 +1,93 @@
+<div class="borderer"></div>
+<div class="padding-20 font-roman yellow line-onehalf">
+    <div class="bold underline text-center font-size-16">
+        BERITA ACARA PEMASANGAN TUGU / TANDA BATAS
+    </div>
+
+    <br/>
+
+    <div class="text-justify">
+
+    </div>
+
+    <table>
+        <colgroup>
+            <col width="">
+            <col width="20">
+            <col width="">
+        </colgroup>
+        
+        <tr>
+            <td class="menjorok">Nama</td>
+            <td class="text-center">:</td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td class="menjorok">Umur</td>
+            <td class="text-center">:</td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td class="menjorok">Alamat</td>
+            <td class="text-center">:</td>
+            <td>
+            </td>
+        </tr>
+    </table>
+
+    <div class="text-justify">
+        Saya selaku pemilik tanah telah melaksanakan pemasangan tanda-tanda batas berupa (Tugu-tugu/besi) pada semua sudut bidang tanah terletak di :
+    </div>
+
+    <ol class="text-justify">
+        <li>
+            Bahwa tanda-tanda batas yang telah terpasang telah mendapat persetujuan dari pemilik tanah yang berbatasan.
+        </li>
+        <li>
+            Bahwa untuk kesaksian tersebut para pemilik tanah yang berbatasan menandatangani Berita Acara ini.
+        </li>
+        <li>
+            Bahwa tanah tersebut belum pernah diajukan permohonan sertipikat baik sebagian atau seluruhnya.
+        </li>        
+    </ol>
+
+    <div>
+        Demikian Berita Acara ini, dibuat dengan sebenar-benarnya tanpa paksaan.
+    </div>
+
+    <br/>
+
+    
+
+    <div class="float-left width-60">
+        <div class="text-center">
+            Para pemilik tanah yang berbatasan
+        </div>
+
+        <br/>
+
+        <div>
+            Saksi-saksi / Perangkat Desa
+        </div>
+
+    </div>
+
+    <div class="float-right width-40">
+        <div class="text-center">
+            Pemohon,
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div class="bold uppercase text-center"></div>
+    </div>
+
+    <div class="clear"></div>
+    <br/>
+    
+
+    <div class="clear"></div>
+</div>
