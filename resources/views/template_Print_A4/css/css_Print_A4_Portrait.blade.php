7d556b1c156c4a538b53a0ac1b620141e2e058ab
Phoenix3264 <herlambangharyo@gmail.com>
2020-08-21 11:38:11 +0700

    Update css_Print_A4_Portrait.blade.php

--- resources/views/template_Print_A4/css/css_Print_A4_Portrait.blade.php
+++ resources/views/template_Print_A4/css/css_Print_A4_Portrait.blade.php
@@ -2,3 +2,9 @@
 
 <link href="{{ asset('/public/').'/'.$root }}/assets/plugins/jquery-simplecolorpicker/jquery.simplecolorpicker-glyphicons.css" rel="stylesheet" />
 <link href="{{ asset('/public/').'/print' }}/A4_Portrait_print.css" rel="stylesheet" />
+
+
+<link rel="stylesheet" type="text/css" href="{{ asset('/public/print') }}/bothw.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('/public/print') }}/A4_Portrait_print.css" media="print">
+<link rel="stylesheet" type="text/css" href="{{ asset('/public/print') }}/chrome.css" >
+<link rel="stylesheet" type="text/css" href="{{ asset('/public/print') }}/A4_Portrait_screen.css" media="screen">
